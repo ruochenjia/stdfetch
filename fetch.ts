4f0714b0b6e1524265079e99f23c88e39177187c
Ruochen Jia <rjia@rjia.gq>
2023-01-29 22:39:25 +0000

    better headers implementation

--- fetch.ts
+++ fetch.ts
@@ -16,12 +16,12 @@ const zeroArrayBuffer = new ArrayBuffer(0);
 const zeroBuffer = Buffer.from(zeroArrayBuffer, 0, 0);
 const pipeline = util.promisify(stream.pipeline);
 
-class FetchError extends Error {}
-class LogicError extends Error {}
+class FetchError extends Error { }
+class LogicError extends Error { }
 class Cloneable extends null {
 	clone(): this { throw new Error("Function not implemented"); }
 }
-Object.setPrototypeOf(Cloneable, class _Null_ {});
+Object.setPrototypeOf(Cloneable, class _Null_ { });
 
 type nul = null | undefined | void;
 type RequestCredentials = "include" | "omit" | "same-origin"
@@ -34,19 +34,13 @@ type ResponseType = "basic" | "cors" | "default" | "error" | "opaque" | "opaquer
 type DataInit = ArrayBufferLike | ArrayLike<number>;
 type SourceInit = DataInit | string | Buffer;
 type BodyInit = stream.Readable | SourceInit | Body;
-
-interface MapLike<E> {
-	[k: string]: E;
-}
-
-interface Headers extends MapLike<string> {
-}
+type HeadersInit = [string, string][] | Record<string, string> | Headers;
 
 interface RequestInit {
 	readonly body?: BodyInit | nul;
 	readonly cache?: RequestCache | nul;
 	readonly credentials?: RequestCredentials | nul;
-	readonly headers?: Headers | nul;
+	readonly headers?: HeadersInit | nul;
 	readonly integrity?: string | nul;
 	readonly keepalive?: boolean | nul;
 	readonly method?: string | nul;
@@ -57,12 +51,87 @@ interface RequestInit {
 }
 
 interface ResponseInit {
-	readonly headers?: Headers | nul;
+	readonly headers?: HeadersInit | nul;
 	readonly redirected?: boolean | nul;
 	readonly status?: number | nul;
 	readonly statusText?: string | nul;
 	readonly type?: ResponseType | nul;
 	readonly url?: string | URL | nul;
+}
+
+export class Headers extends Cloneable {
+	readonly get: (key: string) => string;
+	readonly set: (key: string, value: string) => this;
+	readonly has: (key: string) => boolean;
+	readonly delete: (key: string) => boolean;
+	readonly clear: () => void;
+
+	declare readonly keys: string[];
+	declare readonly values: string[];
+	declare readonly entries: [string, string][];
+	declare readonly size: number;
+	declare readonly map: Record<string, string>;
+
+	constructor(init?: HeadersInit | nul) {
+		super();
+		const variables: Record<string, string> = headersInit(init);
+
+		this.get = (key) => {
+			checkHeaderKey(key = key.toLowerCase());
+			return variables[key] || "";
+		};
+
+		this.set = (key, value) => {
+			checkHeaderKey(key = key.toLowerCase());
+			variables[key] = value;
+			return this;
+		};
+
+		this.has = (key) => {
+			return key.toLowerCase() in variables;
+		};
+
+		this.delete = (key) => {
+			checkHeaderKey(key = key.toLowerCase());
+			return delete variables[key];
+		};
+
+		this.clear = () => {
+			for (const k of Object.keys(variables)) {
+				delete variables[k];
+			}
+		};
+
+		Object.defineProperty(this, "keys", {
+			get: () => Object.keys(variables),
+			enumerable: false,
+			configurable: false
+		});
+
+		Object.defineProperty(this, "values", {
+			get: () => Object.values(variables),
+			enumerable: false,
+			configurable: false
+		});
+
+		Object.defineProperty(this, "entries", {
+			get: () => Object.entries(variables),
+			enumerable: false,
+			configurable: false
+		});
+
+		Object.defineProperty(this, "size", {
+			get: () => Object.keys(variables).length,
+			enumerable: false,
+			configurable: false
+		});
+
+		Object.defineProperty(this, "map", {
+			get: () => ({ ...variables }),
+			enumerable: false,
+			configurable: false
+		});
+	}
 }
 
 export class Body extends Cloneable {
@@ -187,7 +256,7 @@ export class Request extends Body implements RequestInit {
 		super(cfg.body);
 		this.cache = cfg.cache || "default";
 		this.credentials = cfg.credentials || "same-origin";
-		this.headers = cfg.headers || {};
+		this.headers = new Headers(cfg.headers);
 		this.integrity = cfg.integrity || "";
 		this.keepalive = cfg.keepalive || false;
 		this.method = cfg.method || "GET";
@@ -211,13 +280,13 @@ export class Response extends Body implements ResponseInit {
 	constructor(body?: BodyInit | nul, init?: ResponseInit) {
 		super(body);
 		const cfg = init || {};
-		const status = cfg.status || 200;	
+		const status = cfg.status || 200;
 		const url = cfg.url;
 
 		if (status < 100 || status > 599)
 			throw new LogicError("Response status must be an integer between 100 and 599");
 
-		this.headers = cfg.headers || {};
+		this.headers = new Headers(cfg.headers);
 		this.ok = status >= 200 && status < 300;
 		this.redirected = cfg.redirected || false;
 		this.status = status;
@@ -227,7 +296,7 @@ export class Response extends Body implements ResponseInit {
 	}
 
 	async writeResponse(outgoing: http.ServerResponse, options?: { end?: boolean | nul; } | nul) {
-		outgoing.writeHead(this.status, this.statusText, this.headers);
+		outgoing.writeHead(this.status, this.statusText, this.headers.map);
 		await this.writeData(outgoing, options);
 	}
 }
@@ -243,8 +312,34 @@ function validateUrl(url: string | URL) {
 	}
 }
 
+function checkHeaderKey(key: string) {
+	for (let i = 0; i < key.length; i++) {
+		const ch = key.charCodeAt(i);
+		if ((ch < 0x30 || ch > 0x39) && (ch < 0x61 || ch > 0x7a) && ch != 0x2d) {
+			throw new LogicError("Invalid header key");
+		}
+	}
+}
+
+function headersInit(init?: HeadersInit | nul): Record<string, string> {
+	if (init == null) {
+		return {};
+	} else if (init instanceof Headers) {
+		return init.map;
+	} else if (init instanceof Array) {
+		const headers: Record<string, string> = {};
+		for (const item of init) {
+			const key = item[0].toLowerCase();
+			const value = item[1];
+			checkHeaderKey(key);
+			headers[key] = value;
+		}
+		return headers;
+	} else return headersInit(Object.entries(init));
+}
+
 async function getStreamBuffer(readable: stream.Readable) {
-	const out = new stream.PassThrough({ });
+	const out = new stream.PassThrough({});
 	const chunks: Uint8Array[] = [];
 	out.on("data", (chunk) => chunks.push(chunk));
 	await pipeline(readable, out);
@@ -257,7 +352,7 @@ function rawFetch(request: Request): Promise<http.IncomingMessage> {
 	const host = url.host;
 	const headers = request.headers;
 
-	headers["host"] = url.host;
+	headers.set("Host", url.host);
 
 	const options: http.RequestOptions = {
 		protocol,
@@ -267,7 +362,7 @@ function rawFetch(request: Request): Promise<http.IncomingMessage> {
 		// Instead, remove the origin from the href
 		path: url.href.substring(url.origin.length),
 		method: request.method,
-		headers,
+		headers: headers.map,
 		setHost: true,
 		timeout: 10000
 	};
@@ -347,20 +442,27 @@ function fetchDataUrl(url: URL) {
 
 	const meta = head.split(";");
 	const mime = meta[0];
+	const encoding = meta[1];
 
-	if (meta[1] != "base64") {
+	if (encoding == "base64") {
+		return new Response(Buffer.from(body, "base64"), {
+			status: 200,
+			headers: { "Content-Type": mime },
+			url
+		});
+	} else if (encoding.startsWith("charset=")) {
+		return new Response(Buffer.from(body, encoding.substring(8) as BufferEncoding), {
+			status: 200,
+			headers: { "Content-Type": mime },
+			url
+		});
+	} else {
 		return new Response(body, {
 			status: 200,
-			headers: { "content-type": mime },
+			headers: { "Content-Type": mime },
 			url
 		});
 	}
-
-	return new Response(Buffer.from(body, "base64"), {
-		status: 200,
-		headers: { "content-type": mime },
-		url
-	});
 }
 
 function fetchJavascriptUrl(url: URL) {
@@ -369,25 +471,22 @@ function fetchJavascriptUrl(url: URL) {
 		eval(data);
 		return new Response(void 0, { status: 200, url });
 	} catch (err) {
-		throw new FetchError("Script execution failed: " + (err instanceof Error ? err.message: err));
+		throw new FetchError("Script execution failed: " + (err instanceof Error ? err.message : err));
 	}
 }
 
 function localFetch(url: URL) {
 	const path = decodeURIComponent(url.pathname);
-	const mime = _mime[Path.extname(path)];
-	const headers: Headers = {};
-	if (mime != null)
-		headers["content-type"] = mime;
+	const mime = _mime[Path.extname(path)] || "application/unknown";
 
 	try {
-		return new Response(fs.readFileSync(path, { }), {
+		return new Response(fs.readFileSync(path, {}), {
 			status: 200,
-			url,
-			headers
+			headers: { "Content-Type": mime },
+			url
 		});
 	} catch (err) {
-		throw new FetchError("Failed to fetch: " + (err instanceof Error ? err.message: err));
+		throw new FetchError("Failed to fetch: " + (err instanceof Error ? err.message : err));
 	}
 }
 
@@ -415,14 +514,13 @@ export default async function fetch(req: RequestInfo, init?: RequestInit | nul) 
 		case "http:":
 		case "https:":
 			const incoming = await remoteFetch(request);
-			const headers = incoming.headers as Headers;
-			const redirectCount = headers["_$0_redirect_count"] || 0;
+			const redirectCount = incoming.headers["_$0_redirect_count"] || 0;
 
 			return new Response(incoming, {
 				status: incoming.statusCode,
 				statusText: incoming.statusMessage,
 				redirected: redirectCount > 0,
-				headers,
+				headers: incoming.headers as Record<string, string>,
 				url
 			});
 		case "file:":
